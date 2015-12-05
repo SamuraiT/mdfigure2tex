a6da8cde61548edabe9775d9539071369f571afc
tatsuro-yasukawa <tatsuro-yasukawa@cookpad.com>
2015-12-05 19:37:36 +0900

    Add template generate code and test

--- lib/FigureTemplate.rb
+++ lib/FigureTemplate.rb
@@ -1,5 +1,40 @@
 require "FigureTemplate/version"
 
 module FigureTemplate
-  # Your code goes here...
+  require 'erb'
+  class Engine
+    include ERB::Util
+    attr_accessor :items, :template, :date
+
+    def initialize(template)
+      @template = template
+    end
+
+    def render()
+      ERB.new(@template).result(binding)
+    end
+
+    def save(file)
+      File.open(file, "w+") do |f|
+        f.write(render)
+      end
+    end
+
+    def figure(image_path, size, caption, label)
+      <<-EOD.gsub(/ /, '')
+
+        \\begin{figure}[htbp]
+        \\centering
+        \\includegraphics[width=#{size[:width]}cm]{#{image_path}}
+        \\caption{#{caption}}
+        \\label{#{label}}
+        \\end{figure}
+      EOD
+    end
+
+    def self.readfile
+      template = ARGV.first
+      Figure.new(template)
+    end
+  end
 end
--- spec/FigureTemplate_spec.rb
+++ spec/FigureTemplate_spec.rb
@@ -1,11 +1,65 @@
 require 'spec_helper'
 
 describe FigureTemplate do
+  let(:template) do
+    <<-EOD
+# markdown
+texの図をレンダーするよ
+<%= figure("image_path", {width: 50, height: 50}, "caption", "label") %>
+  ほげ
+EOD
+  end
+
+  let(:tex_figure) do
+    <<-EOD
+# markdown
+texの図をレンダーするよ
+
+\\begin{figure}[htbp]
+\\centering
+\\includegraphics[width=50cm]{image_path}
+\\caption{caption}
+\\label{label}
+\\end{figure}
+
+  ほげ
+EOD
+  end
+
   it 'has a version number' do
     expect(FigureTemplate::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'figure and render' do
+    let(:template) {
+      "<%= figure('image_path', \
+        {width: 50, height: 10}, \
+        'caption', \
+        'label') %>"
+    }
+
+    let(:tex_figure) {
+      <<-EOD.gsub(/ /, '')
+
+          \\begin{figure}[htbp]
+          \\centering
+          \\includegraphics[width=50cm]{image_path}
+          \\caption{caption}
+          \\label{label}
+          \\end{figure}
+      EOD
+    }
+
+    it 'generate figure correctly' do
+      figure = FigureTemplate::Engine::new(template)
+      expect(figure.render).to eq(tex_figure)
+    end
+  end
+
+  describe 'render' do
+    it 'render' do
+      figure = FigureTemplate::Engine::new(template)
+      expect(figure.render).to eq(tex_figure)
+    end
   end
 end
